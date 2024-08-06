af12712ba651e8f3ec4ce4dbeef15d2136a42d57
BhavyaApprikart <bhavya@apprikart.com>
2024-08-06 15:26:31 +0530

    changes3

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,20 @@
-import { useState } from "react";
-
-import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import Splash from "./pages/Splash";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Splash2 from "./pages/Splash2";
 import Message from "./pages/Message";
 import Register from "./pages/Register";
 
 function App() {
   return (
-    <BrowserRouter>
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path="/" exact element={<Splash />} />
-            <Route path="message" element={<Message />} />
-            <Route path="register" element={<Register />} />
-          </Route>
-        </Routes>
-  </BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Splash />} />
+          <Route path="message" element={<Message />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
