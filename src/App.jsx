17153e296056cd1e7b6c6298cce7a31d7514214f
BhavyaApprikart <bhavya@apprikart.com>
2024-08-06 15:17:32 +0530

    changes2

--- src/App.jsx
+++ src/App.jsx
@@ -11,15 +11,14 @@ import Register from "./pages/Register";
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route  exact path="/" element={<Layout />}>
-          <Route path="/"  element={<Splash />} />
-          <Route path="/Splash2" element={<Splash2 />} />
-          <Route path="/message" element={<Message />} />
-          <Route path="/register" element={<Register />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" exact element={<Splash />} />
+            <Route path="message" element={<Message />} />
+            <Route path="register" element={<Register />} />
+          </Route>
+        </Routes>
+  </BrowserRouter>
   );
 }
 
